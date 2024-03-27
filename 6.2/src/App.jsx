75cedcf865b1171ce247b8065ec02bf67794207a
manuj <manujdixit04@gmail.com>
2024-03-27 19:38:59 +0530

    6.2

--- 6.2/src/App.jsx
+++ 6.2/src/App.jsx
@@ -1,32 +1,67 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import "./App.css";
+// import axios from "axios";
+
+// function App() {
+//   const [todos, settodos] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("https://sum-server.100xdevs.com/todos")
+//       .then(function (response) {
+//         settodos(response.data.todos);
+//       });
+//   }, []);
+
+//   return (
+//     <>
+//       {todos.map((todo) => (
+//         <Todo title={todo.title} description={todo.description} />
+//       ))}
+//     </>
+//   );
+// }
+
+// function Todo({ title, description }) {
+//   return (
+//     <div>
+//       <h1>{title}</h1>
+//       <h2>{description}</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react";
 
 function App() {
-  const [todos, settodos] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-  useEffect(() => {
-    axios
-      .get("https://sum-server.100xdevs.com/todos")
-      .then(function (response) {
-        settodos(response.data.todos);
-      });
-  }, []);
+  let count = 0;
+  for (let i = 1; i <= inputValue; i++) {
+    count = count + i;
+  }
 
-  return (
-    <>
-      {todos.map((todo) => (
-        <Todo title={todo.title} description={todo.description} />
-      ))}
-    </>
-  );
-}
-
-function Todo({ title, description }) {
   return (
     <div>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+      <input
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder={"Find sum from 1 to n"}
+      ></input>
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter ({counter})
+      </button>
     </div>
   );
 }
