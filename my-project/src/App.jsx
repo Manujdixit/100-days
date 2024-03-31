248dec7c7b306a8a7bd75947fbb782ff65ab7dac
manuj <manujdixit04@gmail.com>
2024-03-31 18:45:55 +0530

    tailwind

---
+++ my-project/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <>
+      <div className="grid grid-cols-12">
+        <div className="col-span-12 md:col-span-5  bg-green-400 text-red-500 text-xs rounded">
+          First
+        </div>
+        <div className="col-span-12 md:col-span-5 bg-yellow-500 text-blue-900">
+          First
+        </div>
+        <div className="col-span-12 md:col-span-2 bg-pink-500 text-white">
+          First
+        </div>
+      </div>
+    </>
+  );
+}
