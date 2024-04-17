8885a22a8257b11e6373aa288d92804187769574
manuj <manujdixit04@gmail.com>
2024-04-17 11:04:41 +0530

    z

---
+++ js/auth.middleware.js
@@ -0,0 +1,57 @@
+/**
+
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config.js");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(403).json({
+      message: "Token not found",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    console.error("error verifying token: ", error);
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = authMiddleware;
+
+**/
+
+/**
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
+        index:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
+**/
---
+++ typescripts/src/a.ts
@@ -0,0 +1,13 @@
+type User = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+interface User2 {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+
+function isLegal(user: User) {}
