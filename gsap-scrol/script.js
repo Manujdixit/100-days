95dcfbd315a2e8125ed3da34aed72030b351a5e1
manuj <manujdixit04@gmail.com>
2024-04-27 18:32:53 +0530

    scroll trigger

--- gsap-scrol/script.js
+++ gsap-scrol/script.js
@@ -5,24 +5,50 @@ gsap.from("#page #box", {
   rotate: 360,
 });
 
-gsap.from("#page1 #box", {
+// gsap.from("#page1 #box", {
+//   scale: 0,
+//   duration: 1,
+//   rotate: 360,
+//   scrollTrigger: {
+//     trigger: "#page1 #body",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//   },
+// });
+
+gsap.from("#page2 #box", {
   scale: 0,
   duration: 1,
   rotate: 360,
   scrollTrigger: {
-    trigger: "#page1 #body",
+    trigger: "#page2 #box",
     scroller: "body",
     markers: true,
     start: "top 60%",
   },
 });
 
-gsap.from("#page2 #box", {
-  scale: 0,
-  duration: 1,
-  rotate: 360,
-  scrollTrigger: "#page2 #box",
-  scroller: "body",
-  markers: true,
-  start: "top 60%",
+gsap.from("#page1 h1", {
+  opacity: 0,
+  x: 500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#page1 h1",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+  },
+});
+
+gsap.from("#page1 h2", {
+  opacity: 0,
+  x: -500,
+  duration: 2,
+  scrollTrigger: {
+    trigger: "#page1 h2",
+    scroller: "body",
+    markers: true,
+    start: "top 60%",
+  },
 });
