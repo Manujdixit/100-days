1c51c93cbb60a761c7960f920b5e660aeb58dcbd
manuj <manujdixit04@gmail.com>
2024-04-29 01:45:36 +0530

    a

--- gsap-scrol/script.js
+++ gsap-scrol/script.js
@@ -1,9 +1,9 @@
-gsap.from("#page #box", {
-  scale: 0,
-  delay: 1,
-  duration: 1,
-  rotate: 360,
-});
+// gsap.from("#page #box", {
+//   scale: 0,
+//   delay: 1,
+//   duration: 1,
+//   rotate: 360,
+// });
 
 // gsap.from("#page1 #box", {
 //   scale: 0,
@@ -17,38 +17,64 @@ gsap.from("#page #box", {
 //   },
 // });
 
-gsap.from("#page2 #box", {
-  scale: 0,
-  duration: 1,
-  rotate: 360,
+// gsap.from("#page2 #box", {
+//   scale: 0,
+//   duration: 1,
+//   rotate: 720,
+//   scrollTrigger: {
+//     trigger: "#page2 #box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//     end: "top 30%",
+//     scrub: 2,
+//     pin: true,
+//   },
+// });
+
+// gsap.from("#page1 h1", {
+//   opacity: 0,
+//   x: 500,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: "#page1 h1",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//   },
+// });
+
+// gsap.from("#page1 h2", {
+//   opacity: 0,
+//   x: -500,
+//   duration: 2,
+//   scrollTrigger: {
+//     trigger: "#page1 h2",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//   },
+// });
+
+gsap.to("#page2 h1", {
+  transform: "translateX(-100%)",
   scrollTrigger: {
-    trigger: "#page2 #box",
+    trigger: "#page2",
     scroller: "body",
     markers: true,
-    start: "top 60%",
+    start: "top 0%",
+    end: "top -400%",
+    scrub: 2,
+    pin: true,
   },
 });
 
-gsap.from("#page1 h1", {
-  opacity: 0,
-  x: 500,
-  duration: 2,
-  scrollTrigger: {
-    trigger: "#page1 h1",
-    scroller: "body",
-    markers: true,
-    start: "top 60%",
-  },
+let blocksContainer = document.getElementById("blocks");
+let animation = gsap.to(blocksContainer.querySelectorAll(".blocks"), {
+  scale: 0.95,
 });
 
-gsap.from("#page1 h2", {
-  opacity: 0,
-  x: -500,
-  duration: 2,
-  scrollTrigger: {
-    trigger: "#page1 h2",
-    scroller: "body",
-    markers: true,
-    start: "top 60%",
-  },
-});
+let blocks = document.querySelector("#blocks .blocks");
+
+blocks.addEventListener("mouseenter", () => animation.play());
+blocks.addEventListener("mouseleave", () => animation.reverse());
